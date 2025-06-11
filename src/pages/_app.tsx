dc9a6f9bd9e6e8637270fd20ecc54a67a4757c84
achchala <adeepan@uwaterloo.ca>
2025-06-10 20:55:38 -0400

    Add Google Analytics script to _document.tsx for static export compatibility

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from 'next/app'
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <GoogleAnalytics gaId="G-8LZT78243L" />
     </>
   )
 } 
