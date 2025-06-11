bb6bee6822b7087f96594a1d6c6ec41c506be70c
achchala <adeepan@uwaterloo.ca>
2025-06-10 20:52:20 -0400

    Update Google Analytics to use @next/third-parties

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,11 @@
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-8LZT78243L"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-8LZT78243L', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-8LZT78243L" />
     </>
   )
 } 
