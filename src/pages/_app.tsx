a89bb8b53fec837b4c7ff6943be54b08ddb811fb
achchala <adeepan@uwaterloo.ca>
2025-06-10 21:09:30 -0400

    google analytics component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from 'next/app'
+import GoogleAnalytics from '../components/GoogleAnalytics'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </>
   )
